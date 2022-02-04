5fe8093a57493164b758b7b9267fe5cbd4a09c1a
Ben Pedrick <bpedrick@gmail.com>
2022-02-04 16:04:37 -0600

    server working and connected

---
+++ server/server.js
@@ -0,0 +1,27 @@
+// require express
+// gives us a function
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create an instance of express by calling the function returned above
+// gives us an object
+const app = express();
+
+// Getting the body parser to clear up terminal responses
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 5000;
+
+// Puts the HTML and public folder up to see in broser
+app.use(express.static('server/public'));
+
+
+
+
+
+
+
+// tart up our server
+app.listen(port, function() {
+    console.log('listening on port', port);
+  });
