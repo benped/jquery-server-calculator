bd2327d88dd3500ef7bc3af20f3e7d5d59e863a7
Ben Pedrick <bpedrick@gmail.com>
2022-02-04 20:06:31 -0600

    base mode working with 4 operators. outputting to the dom.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,8 +10,12 @@ function onReady(){
     console.log('JS UP');
     $('#calculate').on('click',sendData);
     $('#add').on('click',sendAdd);
+    $('#sub').on('click',sendSub);
+    $('#div').on('click',sendDiv);
+    $('#mul').on('click',sendMul);
 }
 
+// Sends neccessary data to the server in an object to get maths done. 
 function sendData(){
 
     dataTest.input1 = ($('#input1').val());
@@ -27,12 +31,61 @@ function sendData(){
         }
     }).then(function(response){
         console.log('sendData success!', response);
-        dataTest = [];
+        clearDataTest();
+        console.log('Get answer back is', getAnswerBack());
+
     }).catch(function(response){
         console.log('sendData Failed',response);
     })
     }
 
+// Puts Add in the object that is sent to the dom. 
 function sendAdd(){
     dataTest.operator = 'add';
 }
+
+function sendSub(){
+    dataTest.operator = 'sub';
+}
+
+function sendDiv(){
+    dataTest.operator = 'div';
+}
+
+function sendMul(){
+    dataTest.operator = 'mul';
+}
+
+
+// Clears our output object
+function clearDataTest(){
+    dataTest = {
+        input1: '',
+        operator: '',
+        input2: ''
+    };
+}
+
+
+function renderToDom(response){
+
+    for (let answer of response){
+        $('#response').append(`<p>${answer}</p>`);
+    }
+ 
+}
+
+
+// Get to server to get all of our calculations back
+function getAnswerBack(){
+$.ajax({
+    method: 'GET',
+    url: '/answers'
+}).then(function(response){
+    console.log('getAnswerBack success!', response);
+    renderToDom(response);
+}).catch(function(response){
+    console.log('getAnswerBack Failed',response);
+})
+
+};
--- server/server.js
+++ server/server.js
@@ -15,17 +15,46 @@ const port = 5000;
 // Puts the HTML and public folder up to see in broser
 app.use(express.static('server/public'));
 
+let mathExpressions = [];
+
 app.post('/calculate', function(req, res) {
-    console.log('Request at /quotes was made', req.body);
+    console.log('Request at /calculate was made', req.body);
     res.send('calculate recieved!');
+    mathExpressions.push(calculator(req.body.dataTest));
     // res.sendStatus(500);
   });
 
+app.get('/answers', function(req, res){
+    console.log('Get at /answers was made', req.body);
+    res.send(mathExpressions);
+});
 
+// TODO let it take in an object and do math on it
+function calculator(mathObject){
+    let answer;
+    let operator;
+    switch (mathObject.operator){
+        case 'add':
+            answer = Number(mathObject.input1) + Number(mathObject.input2);
+            operator = '+';
+            break;
+        case 'sub':
+            answer = Number(mathObject.input1) - Number(mathObject.input2); 
+            operator = '-';
+            break;
+        case 'div':
+            answer = Number(mathObject.input1) / Number(mathObject.input2); 
+            operator = '/';
+            break;
+        case 'mul':
+        answer = Number(mathObject.input1) * Number(mathObject.input2); 
+        operator = '*';
+        break;
+    }
+    return `${mathObject.input1} ${operator} ${mathObject.input2} = ${answer}`;
+}
 
-
-
-// tart up our server
+// start up our server
 app.listen(port, function() {
     console.log('listening on port', port);
   });
