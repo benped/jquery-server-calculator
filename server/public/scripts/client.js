8331cfbbe3957bff3f06192254516992dcbc8cb7
Ben Pedrick <bpedrick@gmail.com>
2022-02-05 16:49:11 -0600

    Number parser working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,8 +2,9 @@ $(document).ready(onReady);
 
 let dataTest = {
     input1: '',
-    operator: '',
-    input2: ''
+    // DEPRECATED PARTS OF OBJECT
+    // operator: '',
+    // input2: ''
 };
 
 function onReady(){
@@ -17,19 +18,23 @@ function onReady(){
     $('#clear').on('click',clear);
 }
 
+// ------------------------ SERVER TALKING FUNCTIONS --------------------------------
+
 // Sends neccessary data to the server in an object to get maths done. 
 function sendData(){
     dataTest.input1 = ($('#input1').val());
-    dataTest.input2 = ($('#input2').val());
+    // DEPRECATED
+    // dataTest.input2 = ($('#input2').val());
 
     if (dataTest.input1 === ''){
         alert('Enter Number!');
         return false;
     }
-    if (dataTest.operator === ''){
-        alert("input operation!")
-        return false; 
-    }
+    //  DEPRECATED FROM BASE MODE
+    // if (dataTest.operator === ''){
+    //     alert("input operation!")
+    //     return false; 
+    // }
 
     console.log(dataTest);
     $.ajax({
@@ -48,31 +53,45 @@ function sendData(){
     })
     }
 
+// Get to server to get all of our calculations back
+function getAnswerBack(){
+    $.ajax({
+        method: 'GET',
+        url: '/answers'
+    }).then(function(response){
+        console.log('getAnswerBack success!', response);
+        renderToDom(response);
+    }).catch(function(response){
+        console.log('getAnswerBack Failed',response);
+    })
+
+    };
+
+
+// -------------- BASE FUNCTIONALITY ------------------
 // Puts Add in the object that is sent to the dom. 
-function sendAdd(){
-    dataTest.operator = '+';
-}
+// function sendAdd(){
+//     dataTest.operator = '+';
+// }
 
-function sendSub(){
-    dataTest.operator = '-';
-}
+// function sendSub(){
+//     dataTest.operator = '-';
+// }
 
-function sendDiv(){
-    dataTest.operator = '/';
-    // $('#div').toggle(.addClass('shadow'),.removeClass('shadow'));
-}
+// function sendDiv(){
+//     dataTest.operator = '/';
+//     // $('#div').toggle(.addClass('shadow'),.removeClass('shadow'));
+// }
 
-function sendMul(){
-    dataTest.operator = '*';
-}
+// function sendMul(){
+//     dataTest.operator = '*';
+// }
 
-
+// ======================== DOM FUNCTIONS ==========================
 // Clears our output object
 function clearDataTest(){
     dataTest = {
-        input1: '',
-        operator: '',
-        input2: ''
+        input1: ''
     };
 }
 
@@ -81,7 +100,7 @@ function renderToDom(response){
     $('#response').empty();
 
     for (let mathObject of response){
-        $('#response').append(`<p>${mathObject.input1} ${mathObject.operator} ${mathObject.input2} 
+        $('#response').append(`<p>${mathObject.input1} 
         = ${mathObject.answer}</p>`);
         $('#lastAnswer').empty();
         $('#lastAnswer').append(`${mathObject.answer}`);
@@ -89,38 +108,22 @@ function renderToDom(response){
  
 }
 
+// Puts the value of whatever number/operator is clicked into the input box 
+function addInputValue(){
+    // console.log($(this).val());
+    // console.log('This is input val', $('#input1').val());
+    // console.log(isNaN($(this).val()));
+    // console.log(isNaN($('#input1').val()));
+    let inputString = ($('#input1').val()).toString() + ($(this).val()).toString();
+    // console.log('This is inputString',inputString);
+    // console.log('NaN for inputString',isNaN(inputString));  
+    $('#input1').val(`${inputString}`);
+    // $('#input1').val('DOES IT TAKE ANYTHING?');
+    console.log($('#input1').val());
+}
 
-// Get to server to get all of our calculations back
-function getAnswerBack(){
-$.ajax({
-    method: 'GET',
-    url: '/answers'
-}).then(function(response){
-    console.log('getAnswerBack success!', response);
-    renderToDom(response);
-}).catch(function(response){
-    console.log('getAnswerBack Failed',response);
-})
-
-};
 
 function clear(){
     $('#input1').val('');
     $('#input2').val('');
-}
-
-function addInputValue(){
-    console.log($(this).val());
-    console.log('This is input val', $('#input1').val());
-    console.log(isNaN($(this).val()));
-    console.log(isNaN($('#input1').val()));
-    let inputString = ($('#input1').val()).toString() + ($(this).val()).toString();
-    console.log('This is inputString',inputString);
-    console.log('NaN for inputString',isNaN(inputString));
-    
-    
-    $('#input1').val(`${inputString}`);
-    // $('#input1').val('DOES IT TAKE ANYTHING?');
-    console.log($('#input1').val());
-    
 }
--- server/server.js
+++ server/server.js
@@ -31,31 +31,83 @@ app.get('/answers', function(req, res){
 
 // TODO let it take in an object and do math on it
 function calculator(mathObject){
-    let answer;
-    switch (mathObject.operator){
-        case '+':
-            answer = Number(mathObject.input1) + Number(mathObject.input2);
-            // operator = '+';
-            mathObject.answer = answer; 
-            break;
-        case '-':
-            answer = Number(mathObject.input1) - Number(mathObject.input2); 
-            // operator = '-';
-            mathObject.answer = answer;
-            break;
-        case '/':
-            answer = Number(mathObject.input1) / Number(mathObject.input2); 
-            // operator = '/';
-            mathObject.answer = answer;
-            break;
-        case '*':
-            answer = Number(mathObject.input1) * Number(mathObject.input2); 
-            // operator = '*';
-            mathObject.answer = answer;
-            break;
+    // let answer;
+    // switch (mathObject.operator){
+    //     case '+':
+    //         answer = Number(mathObject.input1) + Number(mathObject.input2);
+    //         // operator = '+';
+    //         mathObject.answer = answer; 
+    //         break;
+    //     case '-':
+    //         answer = Number(mathObject.input1) - Number(mathObject.input2); 
+    //         // operator = '-';
+    //         mathObject.answer = answer;
+    //         break;
+    //     case '/':
+    //         answer = Number(mathObject.input1) / Number(mathObject.input2); 
+    //         // operator = '/';
+    //         mathObject.answer = answer;
+    //         break;
+    //     case '*':
+    //         answer = Number(mathObject.input1) * Number(mathObject.input2); 
+    //         // operator = '*';
+    //         mathObject.answer = answer;
+    //         break;
+    // }
+    // return mathObject; 
+
+
+    // NOTE - this string parser was adapted from here: https://stackoverflow.com/a/16327230
+
+
+        let chars = mathObject.input1.split(""); //Builds array with each char seperated into individual array elements
+        let numbers = []; //Empty array to store integers from chars array
+        let operators = [];   // Empty array to store operators from chars array
+        let index = 0; 
+        let operatorLast = true; //For checking if last char was an operator 
+    
+        numbers[index] = "";
+    
+        // Parses the expression
+        for (let char of chars) {
+            if (isNaN(parseInt(char)) && char !== "." && !operatorLast) { //Checks if char is Not a Number, is not a . and the last char was Not an operator, if all true, it must be a new operator
+                operators[index] = char; 
+                index++;
+                numbers[index] = "";
+                operatorLast = true;
+            } else {
+                numbers[index] += char;
+                operatorLast = false;
+            }
+        }
+
+        console.log(numbers);
+        console.log(operators);
+        
+    
+        // Calculate the expression
+        mathObject.answer = parseFloat(numbers[0]);
+        for (let i = 0; i < operators.length; i++) {
+            var num = parseFloat(numbers[i + 1]);
+            switch (operators[i]) {
+                case "+":
+                    mathObject.answer = mathObject.answer + num;
+                    break;
+                case "-":
+                    mathObject.answer = mathObject.answer - num;
+                    break;
+                case "*":
+                    mathObject.answer = mathObject.answer * num;
+                    break;
+                case "/":
+                    mathObject.answer = mathObject.answer / num;
+                    break;
+            }
+        }
+        console.log(mathObject.answer);
+        console.log(mathObject.input1);
+        return mathObject;
     }
-    return mathObject; 
-}
 
 // start up our server
 app.listen(port, function() {
